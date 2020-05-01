e6571dec86822d7eb14f9afecee40a1b5713ddb1
EC2 Default User <ec2-user@ip-172-31-25-64.us-east-2.compute.internal>
2020-05-01 15:01:05 +0000

    4回目の提出

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,6 +28,10 @@
                 </div>
             </div>
             @endforeach
+            
+            <div class="d-flex justify-content-center mb-5">
+                {{$posts->links()}}
+            </div>
         </div>
         
 @endsection('content')
